16bbfe5fedc92dbc6952aad3554f99b1b98ae6f8
Mikey Reppy <mxreppy@liquidhub.com>
2013-12-10 15:32:31 -0500

    refactored to start using templates

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.shortcuts 
 
 # Create your views here.
 def home_page( request ):
-	return HttpResponse( '<html><title>To-Do lists</title></html>')
+	return render( request, 'home.html' )
+# 	return HttpResponse( '<html><title>To-Do lists</title></html>')
 	
 # home_page = None
