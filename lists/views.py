79a67257c9f95a660d7f27304fc90257f2ff9ea9
Mikey Reppy <mxreppy@liquidhub.com>
2013-12-17 10:25:28 -0500

    fixed validitation which fixed fn tests

--- lists/views.py
+++ lists/views.py
@@ -14,12 +14,20 @@ def home_page( request ):
 def view_list( request, list_id ):
 
 	itemlist = List.objects.get( id=list_id )
+	error = None
 	
 	if request.method == 'POST': 
-		Item.objects.create( text=request.POST['item_text'], list=itemlist )
-		return redirect( '/lists/%d/' % ( itemlist.id, ) )
+		try: 
+			Item.objects.create( text=request.POST['item_text'], list=itemlist )
+			return redirect( '/lists/%d/' % ( itemlist.id, ) )
+		except ValidationError:
+			error = "You can't have an empty list item" 
 
-	return render( request, 'list.html', { 'list': itemlist } )
+	return render( 
+		request, 
+		'list.html', 
+		{ 'list': itemlist, 'error': error } 
+	)
 
 def new_list( request ):
 	itemlist = List.objects.create()
