02dfc904c9c7ba23c6dc4bca1937d0f5b5055963
Mikey Reppy <mxreppy@liquidhub.com>
2013-12-18 10:16:18 -0500

    extra assert about not creating a list unnecessarily

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -67,11 +67,14 @@ class NewListTest( TestCase ):
 		
 	def test_validation_errors_sent_back_to_homepage_template( self ):
 		response = self.client.post( '/lists/new', data={'text':'' } )
+		
 		self.assertEqual( Item.objects.all().count(), 0 )
+		self.assertEqual( List.objects.all().count(), 0 )
+		
 		self.assertTemplateUsed( response, 'home.html' )
 		self.assertContains( response, escape( EMPTY_LIST_ERROR ))
 		self.assertIsInstance( response.context['form'], ItemForm )
-			
+					
 		
 class ListViewTest( TestCase ):
 
